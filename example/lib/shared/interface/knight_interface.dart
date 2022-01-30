95f98ae3d39cbe19b12bb637d89827ec21efab37
Martinhenle <grafik.henle@gmail.com>
2022-01-30 15:35:16 +0100

    Added Empty Inventory

--- example/lib/shared/interface/knight_interface.dart
+++ example/lib/shared/interface/knight_interface.dart
@@ -3,12 +3,14 @@ import 'package:example/shared/player/knight.dart';
 import 'package:flutter/material.dart';
 
 class PlayerInterface extends GameInterface {
+  late Sprite invEmpty;
   late Sprite obstacleOne;
   late Sprite obstacleTwo;
   late Sprite obstacleThree;
 
   @override
   Future<void> onLoad() async {
+    invEmpty = await Sprite.load('blue_button1.png');
     obstacleOne = await Sprite.load('itens/obstacle_one_inventory.png');
     obstacleTwo = await Sprite.load('itens/obstacle_two_inventory.png');
     obstacleThree = await Sprite.load('itens/obstacle_three_inventory.png');
@@ -24,6 +26,9 @@ class PlayerInterface extends GameInterface {
   }
 
   void _drawObstacles(Canvas c) {
+    invEmpty.renderRect(c, Rect.fromLTWH(150, 20, 50, 50));
+    invEmpty.renderRect(c, Rect.fromLTWH(225, 20, 50, 50));
+    invEmpty.renderRect(c, Rect.fromLTWH(300, 20, 50, 50));
     if (gameRef.player != null &&
         (gameRef.player as Knight).containObstacleOne) {
       obstacleOne.renderRect(c, Rect.fromLTWH(150, 20, 50, 50));
